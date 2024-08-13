3fe9fa4e066a507ed569fc766e53901003a4f7d3
Enrique Rocha Benatti <rochabenattienrique@gmail.com>
2024-08-13 09:44:36 -0300

    Changed terminal commands

--- lua/rocha/remap.lua
+++ lua/rocha/remap.lua
@@ -2,6 +2,9 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>gl", vim.cmd.Ex)
 
+vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
+vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
+
 -- Open the terminal in new window (vertical)
 vim.keymap.set("n", "<leader>vt", "<cmd>vert bo 82 split +ter<CR>Acls<Enter>")
 
@@ -11,7 +14,7 @@ vim.keymap.set("n", "<leader>ht", "<cmd>hor bo 12 split +ter<CR>Acls<Enter>")
 -- Close the terminal
 vim.keymap.set("t", "<c-k>", function()  
     if vim.o.buftype == 'terminal'
-        then vim.cmd [[close]] 
+        then vim.cmd [[quit]] 
     end
 end, { remap = true })
 
--- lua/rocha/set.lua
+++ lua/rocha/set.lua
@@ -13,4 +13,6 @@ vim.opt.wrap = false
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+vim.opt.guicursor = "n-v-c-sm-i:block,ci-ve:ver25,r-cr-o:hor20"
+
 vim.g.mapleader = ' ' 
