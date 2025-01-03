5b3f3055057b53e177bd6123407ace1eef8dc11b
rochaenrique <rochabenattienrique@gmail.com>
2025-01-03 12:41:11 -0300

    cleaned up set.lua file

--- lua/rocha/set.lua
+++ lua/rocha/set.lua
@@ -1,21 +1,27 @@
+-- leader
+vim.g.mapleader = " " 
+
+-- numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- tabs
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+-- indent and wrap
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
 
+-- search highlighting
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
--- vim.opt.termguicolors = false
---
---
+
+-- cursor style
 vim.opt.guicursor = "n-v-c-sm-i-ci-ve-sm:block,r-cr-o:hor20"
 
-vim.g.mapleader = " " 
+-- netrw
 vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 1
